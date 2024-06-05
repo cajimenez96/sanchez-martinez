68cf4edfea4e871e1378e7887c06027ce9881714
cajimenez96 <carlosj_utn@hotmail.com>
2024-06-05 11:59:45 -0300

    Add global colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ export default {
     'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
   theme: {
+    fontFamily: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'oscuro': 'var(--color-oscuro)',
+      'carmesi': 'var(--color-marron)',
+      'mandarina': 'var(--color-mandarina)',
+      'naranja': 'var(--color-naranja)',
+      'crema': 'var(--color-crema)',
+      'mapuche': 'var(--color-mapuche)',
+      'blanco': '--color-blanco'
+    },
     extend: {},
   },
   plugins: [
