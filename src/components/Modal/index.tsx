dc1609f98736b4b3d0404bad34d421bf0448ff8c
cajimenez96 <carlosj_utn@hotmail.com>
2024-06-17 10:11:00 -0300

    Fix style popup

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -10,9 +10,9 @@ const Modal = () => {
   return (
     <div className={`${open ? 'fixed' : 'hidden'} bottom-16 right-[20%] z-50 w-[1024px] mx-auto`}>
       <div className="relative md:flex md:items-center md:justify-center w-full">
-          <div className="h-auto md:h-full rounded-lg p-4 fixed inset-x-0 bottom-0 z-50 mx-auto md:relative shadow-lg w-full  border border-crema flex flex-col items-end bg-oscuro-blur backdrop-blur-xl">
+          <div className="h-auto md:h-full rounded-lg p-4 fixed inset-x-0 bottom-0 z-50 mx-auto md:relative shadow-lg w-full  border border-naranja flex flex-col items-end bg-oscuro-blur backdrop-blur-xl">
             <button className="text-right cursor-pointer" onClick={() => setOpen(false)}>
-              <MdOutlineClose className="text-crema" />
+              <MdOutlineClose className="text-crema hover:text-naranja" />
             </button>
             <div className="w-full">
               <div className="md:flex items-center">
@@ -32,12 +32,12 @@ const Modal = () => {
                     buttonStyle="outline"
                     link
                     href={Navigation.contact}
-                    className="text-sm">
+                    className="text-sm border-naranja">
                     Consultar ahora
                   </Button>
                 </div>
                 <div>
-                  <Button buttonStyle="outline" className="text-sm">
+                  <Button buttonStyle="outline" className="text-sm border-naranja">
                     Escríbenos por WhatsApp
                   </Button>
                 </div>
