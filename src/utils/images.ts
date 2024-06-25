6120357619c5b1032bf133c65ae5ddd58173f8cc
Carlos Jimenez <carlos.exejimenez@gmail.com>
2024-06-25 20:08:29 -0300

    Add images to services

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -2,10 +2,14 @@ import logo from '../assets/images/logo.png';
 import serviceImage from '../assets/images/service-image.png';
 import aboutImage from '../assets/images/bg-about.png';
 import firmsImage from '../assets/images/Marca1.png';
+import service1 from '../assets/images/service1.png';
+import service2 from '../assets/images/service2.png';
 
 export const images = {
   logo: logo,
   service: serviceImage,
   about: aboutImage,
-  firm1: firmsImage
+  firm1: firmsImage,
+  serviceConsumption: service1,
+  servicePenal: service2
 };
